46bd34d7e2cab8c84bde87573dbe0802b9731074
doylech <chris.doyle@gmail.com>
2021-02-26 10:53:00 -0800

    Fixed and cleaned up denominator

--- questions/q3.sql
+++ questions/q3.sql
@@ -3,19 +3,15 @@
 
 SELECT 
 	COUNT (DISTINCT psot.patient_id) 
-		FILTER (--Patients who have uncontrolled hypertensions
-			WHERE (psot.diagnosed_disease_state = 'Stage 1'
-				OR psot.diagnosed_disese_state = 'Stage 2'
-				OR psot.diagnosed_disese_state = 'Stage 3')
-			--And ANY of the specified hypertension medications have been increased
-			AND (current_meds.amlodipine > past_meds.amlodipine
+		FILTER (--ANY of the specified hypertension medications have been increased
+			WHERE (current_meds.amlodipine > past_meds.amlodipine
 				OR current_meds.telmisartan > past_meds.telmisartan
 				OR current_meds.losartan > past_meds.losartan
 				OR current_meds.atenolol > past_meds.atenolol
 				OR current_meds.enalapril > past_meds.enalapril
 				OR current_meds.chlorthalidone > past_meds.chlorthalidone
 			    	OR current_meds.hydrochlorothiazide > past_meds.hydrochlorothiazide)
-				--Note: Patients who only have "current_meds.other_bp_medication" increaesd will not be included
+				--Note: Patients who only have "current_meds.other_bp_medication" increased will not be included
 		as numerator,
 					
 	COUNT(DISTINCT psot.patient_id)
@@ -39,9 +35,12 @@ SELECT
 	
 	--Look at hypertensive patients, registered at least 3 months ago, as of a certain date, who have a recent BP observations
 	WHERE psot.calendar_month = '2021/01/31'
-		AND psot.months_since_registration >= 3
-		AND medical_histories.hypertension = 'yes'
+		--Patients who have uncontrolled hypertensions
+		AND (psot.diagnosed_disease_state = 'Stage 1'
+			OR psot.diagnosed_disese_state = 'Stage 2'
+			OR psot.diagnosed_disese_state = 'Stage 3')
 		AND psot.bp_observation_state = 'Less than 3 months'
-		--Do not include patients dead or deleted. (Lost to follow up are excluded above)
+		AND psot.months_since_registration >= 3
+		--Do not include patients dead or deleted. (Lost to follow up are excluded above by having recent BP observation)
 		AND patient.status <> 'dead' --Note: We do not exclude migrated, unresponsive, inactive
 		AND patients.deleted_at = NULL
